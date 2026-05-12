5e4e2e1ea145d80d8554e84965126d948c8fd41e
manish-dev3490 <kmanish87064@gmail.com>
2026-05-12 19:42:09 +0530

    implemented user-connection api

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 const authValidation = require("../middlewares/auth");
 const ConnectionRequestModel = require("../models/connectionRequest");
 const userRouter = express.Router();
+const usersafeData = ["firstName", "lastName", "age", "photo"]
 
 // api to recieve the connection requests
 userRouter.get("/user/requests/recieved", authValidation, async (req, res) => {
@@ -10,7 +11,7 @@ userRouter.get("/user/requests/recieved", authValidation, async (req, res) => {
         const requestrecived = await ConnectionRequestModel.find({
             toUserId: loggedInUser._id,
             status: "intrested",
-        }).populate("fromUserId", ["firstName", "lastName", "age", "photo"]);
+        }).populate("fromUserId", usersafeData);
 
         if (!requestrecived) throw new Error("you have got no connection request");
         res.json({ message: `Data fetched successfully `, data: requestrecived });
@@ -18,5 +19,30 @@ userRouter.get("/user/requests/recieved", authValidation, async (req, res) => {
         res.status(400).send("Error  " + error.message);
     }
 });
+
+// api to see all the connections/friends 
+userRouter.get("/user/connections", authValidation, async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+        const acceptedRequest = await ConnectionRequestModel.find({
+            $or: [{ toUserId: loggedInUser._id, status: "accepted" },
+            { fromUserId: loggedInUser._id, status: "accepted" }
+            ]
+        }).populate("fromUserId", usersafeData).populate("toUserId", usersafeData)
+
+        if (!acceptedRequest) throw new Error("you have no connections");
+        const data = acceptedRequest.map((connection) => {
+            if (connection.fromUserId._id.toString() === loggedInUser._id.toString()) {
+                return connection.toUserId;
+            }
+            else return connection.fromUserId;
+        })
+        res.json({ message: 'Data fetched successfully', data: data })
+    }
+    catch (error) {
+        res.status(400).send("Error ", error.message)
+    }
+
+})
 
 module.exports = userRouter;
