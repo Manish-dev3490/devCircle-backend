c3b59309d1c750f45f3b8a87cdde67e94804fd9d
manish-dev3490 <kmanish87064@gmail.com>
2026-06-16 16:50:17 +0530

    configured the socket io for backend

--- src/app.js
+++ src/app.js
@@ -1,12 +1,8 @@
 const path = require("path");
-
 const result = require("dotenv").config({
   path: path.resolve(__dirname, "../.env"),
 });
-
-
 const express = require("express");
-
 const connectDB = require("./config/database");
 const cookieParser = require("cookie-parser");
 const app = express();
@@ -18,25 +14,29 @@ const userRouter = require('./routes/userRouter');
 const rateLimmiter = require('./middlewares/rateLimmiter');
 const cors = require('cors');
 const AiRouter = require("./routes/AIrouter");
-
+const http=require('http');
+const initializeSocket = require("./helper/socket");
 
 
 app.use(cors({
   origin: "http://localhost:5173",
   credentials: true
 }));
+
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(rateLimmiter)
 
-
+// all the routers are present hre
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", connectionRequestRouter)
 app.use("/", userRouter);
 app.use("/",AiRouter)
 
-
+const server=http.createServer(app);
+initializeSocket(server)
 
 async function initializeConnection() {
 
@@ -47,7 +47,7 @@ async function initializeConnection() {
     await redisClient.connect();
     console.log("connected with redis database");
 
-    app.listen(process.env.PORT, () => {
+    server.listen(process.env.PORT, () => {
       console.log("server is listening on the port 3000");
     })
 
