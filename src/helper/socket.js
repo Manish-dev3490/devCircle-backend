6e620b5c99473b8effb66002089574b91c97b35c
manish-dev3490 <kmanish87064@gmail.com>
2026-06-18 21:52:53 +0530

    created chat feauture

--- src/helper/socket.js
+++ src/helper/socket.js
@@ -12,9 +12,22 @@ const initializeSocket = (server) => {
     io.on("connection", (socket) => {
         console.log("New User Connected:", socket.id);
 
-        socket.on("disconnect", () => {
-            console.log("User Disconnected:", socket.id);
-        });
+
+        // chat ko join krne vaala event hai yah room banega 
+        socket.on("join-chat", ({firstName, firstUserId, toUserId }) => {
+            const roomId = [firstUserId, toUserId].sort().join("_");
+            socket.join(roomId);
+            console.log(firstName + " joined the chat " + roomId);
+        })
+
+
+        // message ko recive krega yeh event 
+        socket.on("send-message", ({ _id, toUserId, firstName, text }) => {
+            console.log(firstName + " is connected and has emitted the event send message and msg is " + text)
+            const roomId=[_id,toUserId].sort().join("_");
+            io.to(roomId).emit("new-message",{firstName,text});
+
+        })
     });
 }
 module.exports = initializeSocket;
