7fb2044f59a055a1a99f84e8c482154e63f8ac6b
manish-dev3490 <kmanish87064@gmail.com>
2026-07-09 21:51:38 +0530

    implemented authentication and authorization in chat feat :

--- src/helper/socket.js
+++ src/helper/socket.js
@@ -1,28 +1,91 @@
 const socket = require("socket.io");
 const chatModel = require("../models/chat")
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+const ConnectionRequestModel = require("../models/connectionRequest");
 
 const initializeSocket = (server) => {
     const io = socket(server, {
         cors: {
-            origin: "http://localhost:5173"
+            origin: "http://localhost:5173",
+            credentials: true
         },
     })
 
+
+    // 👇 Authentication Middleware
+    io.use(async (socket, next) => {
+        try {
+            const cookies = socket.handshake.headers.cookie;
+
+            if (!cookies) {
+                return next(new Error("Token not found"));
+            }
+
+            // cookies = "token=eyJhbGcOiJIUzI1NiIs..."
+            const token = cookies.split("=")[1];
+
+            const payload = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+
+            const user = await User.findById(payload._id);
+            if (!user) return next(new Error("user not found"));
+            socket.user = user;
+
+            next();
+        } catch (error) {
+            console.log(error.message);
+            next(new Error("Unauthorized"));
+        }
+    });
 
     io.on("connection", (socket) => {
         console.log("New User Connected:", socket.id);
 
 
         // chat ko join krne vaala event hai yah room banega 
-        socket.on("join-chat", ({ firstName, firstUserId, toUserId }) => {
-            const roomId = [firstUserId, toUserId].sort().join("_");
-            socket.join(roomId);
-            console.log(firstName + " joined the chat " + roomId);
+        socket.on("join-chat", async ({ firstName, firstUserId, toUserId }) => {
+            try {
+
+
+
+                const connection = await ConnectionRequestModel.findOne({
+                    status: "accepted",
+                    $or: [
+                        {
+                            fromUserId: firstUserId,
+                            toUserId: toUserId
+                        },
+                        {
+                            fromUserId: toUserId,
+                            toUserId: firstUserId
+                        }
+                    ]
+                });
+
+                if (!connection) {
+                    throw new Error("You are not authorized to join this chat");
+                }
+
+                const roomId = [firstUserId, toUserId].sort().join("_");
+
+                socket.join(roomId);
+
+                console.log(`${firstName} joined the chat ${roomId}`);
+
+            }
+            catch (error) {
+                console.log(error.message);
+
+                socket.emit("chat-error", {
+                    message: error.message
+                });
+            }
         })
 
 
         // message ko recive krega yeh event and yaha db mein save karvana hai 
-        socket.on("send-message",async ({ _id, toUserId, firstName, text }) => {
+        socket.on("send-message", async ({ _id, toUserId, firstName, text }) => {
             try {
                 console.log(firstName + " is connected and has emitted the event send message and msg is " + text)
                 const roomId = [_id, toUserId].sort().join("_");
@@ -38,13 +101,12 @@ const initializeSocket = (server) => {
                     })
                 }
 
-                chat.messages.push({senderId:_id,text});
+                chat.messages.push({ senderId: _id, text });
                 await chat.save();
                 io.to(roomId).emit("new-message", { firstName, text });
             }
             catch (error) {
                 console.log(error.message);
-
             }
 
         })
